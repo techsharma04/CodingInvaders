8f825293d3174635a01574a1f0181e8c12a4d8d3
techsharma04 <137366208+techsharma04@users.noreply.github.com>
2023-06-27 22:27:25 -0400

    Add files via upload

---
+++ Assignments/Module 13 Atul Sharma Project/script.js
@@ -0,0 +1,12 @@
+let btnAdd = document.getElementById("btn-add");
+let btnPriority = document.getElementById("btn-priority");
+let btnDelete = document.getElementById("btn-delete");
+let listInput = document.getElementById("list-input").value;
+
+function toDoList(item){
+    let isPriority = false;
+    btnAdd.addEventListener("click", () => {
+    
+    })
+}
+
