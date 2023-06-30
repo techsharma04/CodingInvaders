d2b370a5cd587c56934368eabe10f5c9e3cfabdd
techsharma04 <137366208+techsharma04@users.noreply.github.com>
2023-06-30 14:30:31 -0400

    Update script.js

--- Assignments/Module 13 Atul Sharma Project/Simon Says/script.js
+++ Assignments/Module 13 Atul Sharma Project/Simon Says/script.js
@@ -5,18 +5,23 @@ let btnRed = document.querySelector(".btn-red");
 let btnYellow = document.querySelector(".btn-yellow");
 let btnBlue = document.querySelector(".btn-blue");
 let btnGreen = document.querySelector(".btn-green");
-deActivateButtons(true);
 
 let levelHeading = document.querySelector("h1");
+let timeHeading = document.querySelector("h2");
 let message = document.querySelector("p");
-let section = document.querySelector(".section");
+let section = document.querySelector(".record");
+
+let timeArray = [];
+let tr;
+let date = new Date();
 
 let arr = [];
 let btnClick = [];
 let level = 1;
 let flashTime;
 let leaderBoard = 0;
-let leaderBoardRecord = []
+let leaderBoardArray = [];
+let leaderBoardObj = {};
 
 let colors = ["red", "green", "blue", "yellow"];
 let btnObj = {
@@ -25,25 +30,50 @@ let btnObj = {
   blue: btnBlue,
   green: btnGreen,
 };
+deActivateButtons(true);
+setInterval(myTimer, 1000);
 
+function myTimer() {
+  const date = new Date();
+  timeHeading.innerHTML = d = date.toLocaleTimeString();
+  return d;
+}
 
 //windows onload function - have to display the leaderboard
 window.addEventListener('DOMContentLoaded', () => {
-    
-  const prevScores = JSON.parse(window.localStorage.getItem("record"));
-  if (prevScores && prevScores.length) {
-      //copy of score history to push new score at last
-      scoreRecord = [...prevScores];
-      for (const prevScore of prevScores) {
-          let listLeaderBoard = document.createElement("li");
-  section.appendChild(listLeaderBoard);
-  listLeaderBoard.textContent = `Your score is ${prevScore}`;
-      }
+
+  const oldRecords = JSON.parse(localStorage.getItem("records"));
+  console.log(localStorage.getItem("records"));
+  if (oldRecords) {
+    //copy of score history to push new score at last
+    leaderBoardRecord = [...oldRecords];
   }
+  oldRecords.forEach(element => {
+    tr = document.createElement("tr");
+    section.appendChild(tr);
+    td1 = document.createElement("td");
+    td1.innerHTML = element.startTime;
+    tr.appendChild(td1);
+    td2 = document.createElement("td");
+    td2.innerHTML = element.finishTime;
+    tr.appendChild(td2);
+    td3 = document.createElement("td");
+    td3.innerHTML = element.record;
+    tr.appendChild(td3);
+  });
 });
 
 // Event call by clicking play
 btnPlay.addEventListener("click", async () => {
+  if (timeArray[0] === undefined) {
+    tr = document.createElement("tr");
+    section.appendChild(tr);
+    let td = document.createElement("td");
+    td.innerHTML = myTimer();
+    timeArray.push(myTimer());
+    leaderBoardObj.startTime = myTimer();
+    tr.appendChild(td);
+  }
   message.innerText = "";
   activateButtons(false);
   flashTime = level + 1;
@@ -52,6 +82,7 @@ btnPlay.addEventListener("click", async () => {
     arr.push(randColor);
     await flashing(randColor);
   }
+
   btnPlay.disabled = true;
   btnPlay.classList.add("opacity");
   btnRestart.classList.remove("opacity");
@@ -83,6 +114,7 @@ function flashing(color) {
   });
 }
 
+
 btnRed.addEventListener("click", () => {
   btnClick.push("red");
   userResult(arr[0], btnClick[0]);
@@ -106,18 +138,19 @@ function userResult(arr1, arr2) {
     btnClick.shift();
     if (arr.length === 0 && btnClick.length === 0) {
       success();
-    } 
-  }else {
+    }
+  } else {
     failure();
   }
 }
 
 function success() {
   deActivateButtons(true);
+  btnPlay.classList.add("flashing-buttons");
   level += 1;
   levelHeading.innerText = `Level ${level}`;
-  message.innerText =
-    "You have won this round. Click on the play button to start next round";
+  message.innerHTML =
+    `You have won this round.<br>Click on the play button to start next round`;
   leaderBoard += 1;
   btnPlay.disabled = false;
   btnPlay.classList.remove("opacity");
@@ -125,21 +158,33 @@ function success() {
 
 function failure() {
   deActivateButtons(true);
-  message.innerText = "You Lost this round! Click on restart to start again!";
+  timeArray = [];
+  message.innerHTML = `You Lost this round!<br>Click on restart to start again!`;
+  btnRestart.classList.add("flashing-buttons");
+  btnPlay.classList.remove("flashing-buttons");
   btnPlay.disabled = true;
   btnPlay.classList.add("opacity");
-  leaderBoardRecord.push(leaderBoard);
-  let listLeaderBoard = document.createElement("li");
-  section.appendChild(listLeaderBoard);
-  listLeaderBoard.textContent = `Your score is ${leaderBoard}`;
-  window.localStorage.setItem("record", JSON.stringify(leaderBoard))
+  let td2 = document.createElement("td");
+  td2.innerHTML = myTimer();
+  tr.appendChild(td2);
+  let td3 = document.createElement("td");
+  td3.textContent = `${leaderBoard}`;
+  tr.appendChild(td3);
+  leaderBoardObj.finishTime = myTimer();
+  leaderBoardObj.record = leaderBoard;
+  leaderBoardArray.push(leaderBoardObj);
+  localStorage.setItem("records", JSON.stringify(leaderBoardArray));
 }
 
 btnRestart.addEventListener("click", () => {
+  btnRestart.classList.remove("flashing-buttons");
+  btnPlay.classList.add("flashing-buttons");
   message.innerText = "";
   deActivateButtons(true);
   level = 1;
+  levelHeading.innerText = `Level ${level}`;
   flashTime = level;
+  leaderBoard = 0;
   arr = [];
   btnClick = [];
   btnPlay.disabled = false;
@@ -148,6 +193,4 @@ btnRestart.addEventListener("click", () => {
   btnRestart.disabled = true;
   console.log(level, flashTime, arr);
 });
-
-
 
