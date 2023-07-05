69204e377516036ab65475481f87323c787414d1
techsharma04 <techsharma04@outlook.com>
2023-07-05 15:31:09 -0400

    Fixed all input fields submission together

--- Assignments/Module 14 Atul Sharma Project/assets/signup.js
+++ Assignments/Module 14 Atul Sharma Project/assets/signup.js
@@ -1,3 +1,3 @@
 function redirect() {
-    location.href = "../";
+    location.replace("../");
 }
--- Assignments/Module 14 Atul Sharma Project/script.js
+++ Assignments/Module 14 Atul Sharma Project/script.js
@@ -16,73 +16,77 @@ const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,10}$/;
 let isValid = {};
 
 function redirect() {
-    location.href = "assets/signup.html";
+  location.replace("assets/signup.html");
 }
 
 signUp.addEventListener("submit", (action) => {
-    action.preventDefault(); // Prevent from submitting the form on enter click
-    let dataArray = Array.from(action.target); // Putting all the values in an array
+  action.preventDefault(); // Prevent from submitting the form on enter click
+  let dataArray = Array.from(action.target); // Putting all the values in an array
 
-    const valName = dataArray[0].value;
-    const valEmail = dataArray[1].value;
-    const valPass = dataArray[2].value;
-    const valCpass = dataArray[3].value;
+  const valName = dataArray[0].value;
+  const valEmail = dataArray[1].value;
+  const valPass = dataArray[2].value;
+  const valCpass = dataArray[3].value;
 
-    // Handling name field with Validations
-    if (valName.length === 0) {
-        nameError.textContent = "Name cannot be empty";
-    } else if (valName.length < 3 || valName.length > 20) {
-        nameError.textContent = "Name must be between 3 - 20 characters";
-    } else if (nameRegex.test(valName) === false) {
-        nameError.textContent = "Invalid name";
-    } else {
-        nameError.textContent = "";
-        isValid.name = "true";
-        document.getElementById("email").focus();
-    }
+  // Handling name field with Validations
+  if (valName.length === 0) {
+    nameError.textContent = "Name cannot be empty";
+  } else if (valName.length < 3 || valName.length > 20) {
+    nameError.textContent = "Name must be between 3 - 20 characters";
+  } else if (nameRegex.test(valName) === false) {
+    nameError.textContent = "Invalid name";
+  } else {
+    nameError.textContent = "";
+    isValid.name = "true";
+    document.getElementById("email").focus();
 
     // Handling email field with Validations
     if (valEmail.length === 0) {
-        emailError.textContent = "Email cannot be empty";
+      emailError.textContent = "Email cannot be empty";
     } else if (emailRegex.test(valEmail) === false) {
-        emailError.textContent = "Invalid email";
+      emailError.textContent = "Invalid email";
     } else {
-        emailError.textContent = "";
-        isValid.email = "true";
-        document.getElementById("password").focus();
-    }
+      emailError.textContent = "";
+      isValid.email = "true";
+      document.getElementById("password").focus();
 
-    // Handling password field with Validations
-    if (valPass.length === 0) {
+      // Handling password field with Validations
+      if (valPass.length === 0) {
         passError.textContent = "Password cannot be empty";
-    } else if (valPass.length < 3 || valPass.length > 10) {
+      } else if (valPass.length < 3 || valPass.length > 10) {
         passError.textContent = "Password must be between 3 - 10 characters";
-    } else if (passRegex.test(valPass) === false) {
+      } else if (passRegex.test(valPass) === false) {
         passError.textContent = "Invalid password";
-    } else {
+      } else {
         passError.textContent = "";
         isValid.pass = "true";
         document.getElementById("confirm-password").focus();
-    }
 
-    // Handling confirm password field with Validations
-    if (valCpass.length === 0) {
-        cPassError.textContent = "Confirm password cannot be empty";
-    } else if (valCpass.length < 3 || valCpass.length > 10) {
-        cPassError.textContent = "Confirm password must be between 3 - 10 characters";
-    } else if (passRegex.test(valCpass) === false) {
-        cPassError.textContent = "Invalid confirm password";
-    } else if (valPass !== valCpass) {
-        passError.textContent = "Passwords don't match";
-        cPassError.textContent = "Passwords don't match";
-    } else {
-        cPassError.textContent = "";
-        isValid.cpass = "true";
+        // Handling confirm password field with Validations
+        if (valCpass.length === 0) {
+          cPassError.textContent = "Confirm password cannot be empty";
+        } else if (valCpass.length < 3 || valCpass.length > 10) {
+          cPassError.textContent =
+            "Confirm password must be between 3 - 10 characters";
+        } else if (passRegex.test(valCpass) === false) {
+          cPassError.textContent = "Invalid confirm password";
+        } else if (valPass !== valCpass) {
+          passError.textContent = "Passwords don't match";
+          cPassError.textContent = "Passwords don't match";
+        } else {
+          cPassError.textContent = "";
+          isValid.cpass = "true";
+        }
+      }
     }
-
-    // Verifying all fields...
-    if (isValid.name === "true" && isValid.email === "true" && isValid.pass === "true" && isValid.cpass === "true") {
-        redirect();
-    }
-})
-
+  }
+  // Verifying all fields...
+  if (
+    isValid.name === "true" &&
+    isValid.email === "true" &&
+    isValid.pass === "true" &&
+    isValid.cpass === "true"
+  ) {
+    redirect();
+  }
+});
