e086ffe977c6afd898b3e449d0496e9c14989394
techsharma04 <techsharma04@outlook.com>
2023-07-10 01:48:48 -0400

    Module 15 Assignment 3 finish

--- Assignments/Module 15 Atul Sharma Project/Assignment15-Q3-Atul Sharma/script.js
+++ Assignments/Module 15 Atul Sharma Project/Assignment15-Q3-Atul Sharma/script.js
@@ -2,55 +2,68 @@ const name = document.querySelector("#cast-input-name");
 const fromCity = document.querySelector("#cast-input-from-city");
 const toCity = document.querySelector("#cast-input-to-city");
 const bookBtn = document.querySelector("#cast-btn");
+const searchBtn = document.querySelector("#search-btn");
 
 class Booking {
-  constructor(){
-      this.items = [];
+  constructor() {
+    this.items = [];
   }
 
-  enqueue(element){
-      return this.items.push(element);
+  enqueue(element) {
+    return this.items.push(element);
   }
 
   dequeue() {
-      if(this.items.length > 0) {
-          return this.items.shift();
-      }
-      else {
-          console.log("Stack is empty");
-      }
+    if (this.items.length > 0) {
+      return this.items.shift();
+    } else {
+      console.log("Stack is empty");
+    }
   }
 
-  front(){
-      return this.items[0];
+  front() {
+    return this.items[0];
   }
 
-   rear(){
-      return this.items[this.items.length - 1];
+  rear() {
+    return this.items[this.items.length - 1];
   }
 
-  isEmpty(){
-      return this.items.length === 0;
+  isEmpty() {
+    return this.items.length === 0;
   }
 
   size() {
-      return this.items.length;
+    return this.items.length;
   }
 }
 
-
 let busBooking = new Booking();
 
-let dataObj = {};
-
 bookBtn.addEventListener("click", () => {
-  const r = Math.floor(Math.random() * 10000) + 100;
-  dataObj.ticketId = r;
-  dataObj.name =name.value;
-  dataObj.fromCity =fromCity.value;
-  dataObj.toCity =toCity.value;
-
-  busBooking.enqueue(dataObj);
-
+  let ticketID = Math.floor(Math.random() * 10000);
+  const ticketDetails = {};
+  ticketDetails.ticketID = ticketID;
+  ticketDetails.userDetails = {
+    name: name.value,
+    fromCity: fromCity.value,
+    toCity: toCity.value,
+  };
+  busBooking.enqueue(ticketDetails);
   console.log(busBooking.items);
-})
+});
+
+function searchUser(element) {
+  busBooking.items.find((value, index) => {
+    let id = busBooking.items[index].ticketID;
+    if(element === id){
+      console.log(busBooking.items[index].userDetails);
+      document.getElementById("output").innerHTML = `<table><tr><th>Name:</th><td>${busBooking.items[index].userDetails.name}</td></tr><tr><th>From City:</th> <td>${busBooking.items[index].userDetails.fromCity} </td></tr><tr><th>To City: </th> <td>${busBooking.items[index].userDetails.toCity}</td></tr></table>`;
+    } 
+  })
+}
+
+searchBtn.addEventListener("click", () => {
+  let ticketID = parseInt(document.getElementById("search-input").value);
+  searchUser(ticketID);
+});
