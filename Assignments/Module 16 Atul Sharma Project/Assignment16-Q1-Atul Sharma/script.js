97dcc94d8d947bf058ff31c39b9e31160cb9f391
techsharma04 <137366208+techsharma04@users.noreply.github.com>
2023-07-20 09:05:40 -0400

    New update files only

---
+++ Assignments/Module 16 Atul Sharma Project/Assignment16-Q1-Atul Sharma/script.js
@@ -0,0 +1,27 @@
+
+// Using timeout function to load the page before the prompt.
+setTimeout(() => {
+const billAmount = prompt("Please enter the total bill amount");
+
+let calculate = () => {
+    let amount = Number(billAmount);
+    let message;
+    if (amount >= 5000) {
+        amount = amount - amount * (20 / 100);
+        message = `The bill amount was $${billAmount}. \nDiscount Applied: 20%. \nTotal amount to be paid: $${amount}.`;
+    } else if (amount >= 3000 && amount < 5000) {
+        amount = amount - amount * (15 / 100);
+        message = `The bill amount was $${billAmount}. \nDiscount Applied: 15%. \nTotal amount to be paid: $${amount}.`;
+    } else if (amount >= 2000 && amount < 3000) {
+        amount = amount - amount * (10 / 100);
+        message = `The bill amount was $${billAmount}. \nDiscount Applied: 10%. \nTotal amount to be paid: $${amount}.`;
+    } else {
+        message = `The bill amount was $${billAmount}. \nDiscount Applied: 0%. \nTotal amount to be paid: $${amount}.`;
+    }
+    return message;
+}
+
+let msg = calculate();
+console.log(msg);
+alert(`${msg} \n\nOutput is visible in the console as well.`);
+}, 500);
